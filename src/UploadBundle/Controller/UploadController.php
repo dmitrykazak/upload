288614fdfdc1b3aada7f58c8215890e903a9c0b8
dmitrykazak <Demon1k@tut.by>
2017-03-28 19:07:29 +0300

    Remove idea

--- src/UploadBundle/Controller/UploadController.php
+++ src/UploadBundle/Controller/UploadController.php
@@ -28,41 +28,4 @@ class UploadController extends Controller
 
       return new Response('Created product id '.$product->getId());
     }
-
-  /**
-   * @Route("/upload")
-   */
-    public function indexUpload()
-    {
-      // Create and configure the reader
-      $file = new \SplFileObject('products.csv');
-
-      $reader = new CsvReader($file, ';');
-
-      $reader->setHeaderRowNumber(0);
-
-
-      $dm = $this->get('doctrine_mongodb')->getManager();
-
-        $document = new DoctrineReader($dm, 'UploadBundle\Document\Product');
-
-        $doctrineWriter = new DoctrineWriter($reader1, 'UploadBundle\Document\Product');
-
-        echo "<pre>";
-        print_r($doctrineWriter->getFields());
-        die();
-
-      foreach ($reader as $row) {
-        $product = new Product();
-        $product->setName($row["product"]);
-        $product->setPrice($row["price"]);
-        $product->setIsDiscontinued($row["discontinued"]);
-
-        $dm->persist($product);
-      }
-
-      $dm->flush();
-
-      return new Response($reader->count());
-    }
 }
